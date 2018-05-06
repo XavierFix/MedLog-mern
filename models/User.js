dd3bbdbd8f82841b06ffc181164fd93d0a18736a
XavierFix <javiereduardorobertiv@gmail.com>
2018-05-06 14:36:05 -0500

    updated styling for chart

--- models/User.js
+++ models/User.js
@@ -15,12 +15,18 @@ const UserSchema = new Schema({
   firstname: {
     type: String,
     trim: true,
+<<<<<<< HEAD
     // required: 'First name is required',
+=======
+>>>>>>> 608e453 (updated styling for chart)
   },
   lastname: {
     type: String,
     trim: true,
+<<<<<<< HEAD
     // required: 'Last name is required',
+=======
+>>>>>>> 608e453 (updated styling for chart)
   },
   // `username` must be of type String
   // `username` will trim leading and trailing whitespace before it's saved
@@ -95,12 +101,37 @@ const UserSchema = new Schema({
       ref: 'HealthLog',
     },
   ],
+  // `prescriptions` is an array that stores ObjectIds
+  // The ref property links these ObjectIds to the Note model
+  // This allows us to populate the User with any associated prescriptions
   prescriptions: [
     {
       // Store ObjectIds in the array
       type: Schema.Types.ObjectId,
       // The ObjectIds will refer to the ids in the Note model
       ref: 'Prescription',
+    },
+  ],
+    // `attachment` is an array that stores ObjectIds
+  // The ref property links these ObjectIds to the Note model
+  // This allows us to populate the User with any associated attachment
+  attachments: [
+    {
+      // Store ObjectIds in the array
+      type: Schema.Types.ObjectId,
+      // The ObjectIds will refer to the ids in the Note model
+      ref: 'Attachement',
+    },
+  ],
+    // `symptoms` is an array that stores ObjectIds
+  // The ref property links these ObjectIds to the Note model
+  // This allows us to populate the User with any associated symptoms
+  symptoms: [
+    {
+      // Store ObjectIds in the array
+      type: Schema.Types.ObjectId,
+      // The ObjectIds will refer to the ids in the Note model
+      ref: 'Symptoms',
     },
   ],
 });
